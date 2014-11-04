17654380eee9c3e6cb3a8a851121db4a90fbd7d1
Michael Woolnough <michael.woolnough@gmail.com>
2014-11-04 16:18:02 +0000

    Moved testDB to separate file

--- drivers_test.go
+++ drivers_test.go
@@ -1,42 +1,6 @@
 package main
 
-import (
-	"io/ioutil"
-	"os"
-	"path"
-	"testing"
-
-	"code.google.com/p/go-sqlite/go1/sqlite3"
-)
-
-type testDB struct {
-	DB
-	tempDir string
-}
-
-func NewTestDB(dirname string, initFn func(*DB) error) (*testDB, error) {
-	tDir, err := ioutil.TempDir(os.TempDir(), "addTests")
-	if err != nil {
-		return nil, err
-	}
-	t := &testDB{
-		tempDir: tDir,
-	}
-	t.Conn, err = sqlite3.Open(path.Join(t.tempDir, "test.db"))
-	if err != nil {
-		return nil, err
-	}
-	err = initFn(&t.DB)
-	if err != nil {
-		return nil, err
-	}
-	return t, nil
-}
-
-func (t *testDB) CleanUp() {
-	t.Close()
-	os.RemoveAll(t.tempDir)
-}
+import "testing"
 
 func TestDriverSet(t *testing.T) {
 	d, err := NewTestDB("setTests", (*DB).prepareDriverStatements)
