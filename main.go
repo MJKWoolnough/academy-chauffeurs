8e7a490fb5edfbbbe84be3fc419611d5edecfd31
Michael Woolnough <michael.woolnough@gmail.com>
2014-11-24 13:45:44 +0000

    Set-up server

--- main.go
+++ main.go
@@ -1,16 +1,44 @@
 package main
 
+import (
+	"fmt"
+	"net"
+	"net/http"
+)
+
 func main() {
 	//Load Config??
+	db, err := SetupDatabase("test.db")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	//Setup Database
+	s := NewServer(db)
 
-	//Start Server
+	http.HandleFunc("/drivers", s.drivers)
+	http.HandleFunc("/adddriver", s.addDriver)
+	http.HandleFunc("/updatedriver", s.updateDriver)
+	http.HandleFunc("/removedriver", s.removeDriver)
 
+	l, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	done := make(chan struct{})
+	go func() {
+		err := http.Serve(l, nil)
+		if err != nil {
+			fmt.Println(err)
+		}
+		close(done)
+	}()
 	//Start Browser
 
-	//Stop Server
+	OpenBrowser("http://127.0.0.1:8080/drivers").Run()
 
-	//End
-
+	l.Close()
+	<-done
 }
