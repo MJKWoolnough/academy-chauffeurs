e7c513d696d47eec858217de8f1ed13a714476ee
Michael Woolnough <michael.woolnough@gmail.com>
2014-12-06 15:30:01 +0000

    Fixed pagination

--- utils.go
+++ utils.go
@@ -5,6 +5,11 @@ import (
 	"strconv"
 )
 
+const (
+	endShow  = 3
+	surround = 3
+)
+
 type Pagination struct {
 	CurrPage, NumPages uint
 }
@@ -14,27 +19,30 @@ func (p Pagination) HTML() template.HTML {
 		return ""
 	}
 	html := "<div class=\"pagination\">"
-	if p.CurrPage == 1 {
+	if p.CurrPage == 0 {
 		html += "<a>Prev</a> "
 	} else {
-		html += "<a href=\"?page=" + strconv.Itoa(int(p.CurrPage)-1) + "\">Prev</a>"
+		html += "<a href=\"?page=" + strconv.Itoa(int(p.CurrPage)-1) + "\">Prev</a> "
 	}
 	elipses := true
-	for page := 1; page <= p.NumPages; page++ {
-		if page < 3 || page > p.NumPages-3 || page >= p.CurrPage-3 || page <= p.CurrPage+3 || p.CurrPage-3 <= 4 || p.CurrPage+3 >= p.NumPages-4 {
-			numStr := strconv.Itoa(page)
+	for page := uint(0); page < p.NumPages; page++ {
+		numStr := strconv.Itoa(int(page + 1))
+		if page == p.CurrPage {
+			html += "<a>" + numStr + "</a> "
+		} else if page < endShow || page >= p.NumPages-endShow || (int(page) >= int(p.CurrPage)-surround && page <= p.CurrPage+surround) || (p.CurrPage-surround-1 == endShow && page == endShow) || (p.CurrPage+surround+1 == p.NumPages-endShow-1 && page == p.NumPages-endShow-1) {
 			html += "<a href=\"?page=" + numStr + "\">" + numStr + "</a> "
 			elipses = true
 		} else if elipses {
 			elipses = false
-			out += "..."
+			html += "... "
 		}
 	}
 
-	if p.CurrPage == p.NumPages {
+	if p.CurrPage == p.NumPages-1 {
 		html += "<a>Next</a> "
 	} else {
 		html += "<a href=\"?page=" + strconv.Itoa(int(p.CurrPage)+1) + "\">Next</a>"
 	}
 	html += "</div>"
+	return template.HTML(html)
 }
