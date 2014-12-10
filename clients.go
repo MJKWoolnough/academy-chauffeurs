2eb601e0ea88b2822b0f5d2a178f4836d245a0f2
Michael Woolnough <michael.woolnough@gmail.com>
2014-12-10 17:18:29 +0000

    Added Clients

---
+++ clients.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/MJKWoolnough/store"
+)
+
+const clientsPerPage = 20
+
+type clientErrors struct {
+	Client
+	NameError, CompanyNameError, AddressError, ReferenceError, PhoneError string
+}
+
+type ClientListPageVars struct {
+	clients []Client
+	Pagination
+}
+
+func (s *Server) clients(w http.ResponseWriter, r *http.Request) {
+	clients := make([]Client, clientsPerPage)
+	data := make([]store.Interface, clientsPerPage)
+	for i := 0; i < clientsPerPage; i++ {
+		data[i] = &clients[i]
+	}
+	n, err := s.list(w, r, data, "clients.html", func(n int, p Pagination) interface{} {
+		for i := 0; i < n; i++ {
+			clients[i].companyName(s.db)
+		}
+		return ClientListPageVars{
+			clients[:n],
+			p,
+		}
+	})
+}
+
+func (s *Server) addClient(w http.ResponseWriter, r *http.Request) {
+	var c clientErrors
+	s.add(w, r, &c, func() bool {
+		good := true
+		if c.Name == "" {
+			good = false
+			c.NameError = "Client Name required"
+		}
+		if c.Address == "" {
+			good = false
+			c.AddressError = "Address required"
+		}
+		if c.Reference == "" {
+			good = false
+			c.ReferenceError = "Reference required"
+		}
+		if c.Phone == "" {
+			good = false
+			c.PhoneError = "Valid Phone Number Required"
+		}
+		c.companyID(s.db)
+		if c.CompanyID == 0 {
+			good = false
+			c.CompanyNameError = "Unknown Company"
+		}
+		return good
+	}, "clients", "clientAdd.html")
+}
+
+func (s *Server) removeClient(w http.ResponseWriter, r *http.Request) {
+	var c Client
+	s.remove(w, r, &c, "clients.html", "clientRemoveConfirmation.html", "clientRemove.html")
+}
+
+func (s *Server) updateClient(w http.ResponseWriter, r *http.Request) {
+	var c clientErrors
+	s.update(w, r, &c, func() bool {
+		good := true
+		if c.Name == "" {
+			good = false
+			c.NameError = "Client Name required"
+		}
+		if c.Registration == "" {
+			good = false
+			c.AddressError = "Address required"
+		}
+		if c.Referencee == "" {
+			good = false
+			c.ReferenceError = "Reference required"
+		}
+		if c.Phone == "" {
+			good = false
+			c.PhoneError = "Phone Number Required"
+		}
+		c.companyID(s.db)
+		if c.CompanyID == 0 {
+			good = false
+			c.CompanyNameError = "Unknown Company"
+		}
+		return good
+	}, "clients", "clientUpdate.html")
+}
