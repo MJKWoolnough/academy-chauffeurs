13b3ae0232e18cffba3a31f6c7872f934a71c8ce
Michael Woolnough <michael.woolnough@gmail.com>
2015-02-28 18:39:02 +0000

    Added initial calls

--- calls.go
+++ calls.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/rpc"
+	"sync"
+	"time"
 
 	"github.com/MJKWoolnough/store"
 )
@@ -23,29 +25,57 @@ type Client struct {
 }
 
 type Event struct {
-	ID     int64
-	Driver Driver
-	Client Client
+	ID         int64
+	Start, End time.Time
+	Driver     Driver
+	Client     Client
+}
+
+type search struct {
+	ps     *store.PreparedSearch
+	params interface{}
 }
 
 type Calls struct {
-	s *store.Store
+	s        *store.Store
+	searches map[string]search
 }
 
-func newCalls(dbFName string) (Calls, error) {
+func newCalls(dbFName string) (*Calls, error) {
 	s, err := store.New(dbFName)
 	if err != nil {
-		return Calls{}, err
+		return nil, err
 	}
 	err = s.Register(new(Event))
 	if err != nil {
-		return Calls{}, err
+		return nil, err
 	}
-	c := Calls{s}
+	c := &Calls{
+		s,
+		make(map[string]search),
+	}
 	err = rpc.Register(c)
 	if err != nil {
-		return Calls{}, err
+		return nil, err
 	}
+	// setup searches
+
+	ef := new(eventsFilter)
+	ns := s.NewSearch(new(Event))
+	ns.Sort = []store.SortBy{{Column: "Start", Asc: true}}
+	ns.Filter = store.And{
+		idEqual{"ID", &ef.DriverID},
+		store.Or{
+			betweenTime{"Start", &ef.From, &e.To},
+			betweenTime{"End", &ef.From, &e.To},
+		},
+	}
+	ps, err := ns.Prepare()
+	if err != nil {
+		return nil, err
+	}
+	c.searches["events"] = search{ps, e}
+
 	return c, nil
 }
 
@@ -53,7 +83,85 @@ func (c Calls) close() {
 	c.s.Close()
 }
 
-func (c Calls) Test(testID *int, response *int) error {
-	*response = *testID + 1
+type eventsFilter struct {
+	DriverID int64
+	From, To time.Time
+	mu       sync.Mutex
+}
+
+func (c Calls) Events(f eventsFilter, eventList *[]Event) error {
+	s := c.searches["events"]
+	n, err := s.ps.Count()
+	if err != nil {
+		return err
+	}
+	e := s.params.(*eventsFilter)
+	e.mu.Lock()
+	defer e.mu.Unlock()
+	e.DriverID = f.DriverID
+	e.From = f.From
+	e.To = f.To
+	es := make([]Event, n)
+	ei := make([]interface{}, 0, n)
+	for i := 0; i < n; i++ {
+		ei = append(ei, &es[i])
+	}
+	_, err = s.ps.GetPage(ei, 0)
+	if err != nil {
+		return err
+	}
+	*eventList = es
 	return nil
+}
+
+func (c Calls) Drivers(_ byte, drivers *[]Driver) error {
+	n, err := c.s.Count(new(Driver))
+	if err != nil {
+		return err
+	}
+	d := make([]Driver, n)
+	di := make([]interface{}, 0, n)
+	for i := 0; i < n; i++ {
+		di = append(di, &d)
+	}
+	_, err = c.s.GetPage(di, 0)
+	if err != nil {
+		return err
+	}
+	*drivers = d
+	return nil
+}
+
+func (c Calls) SetDriver(d Driver, id *int64) error {
+	err := c.s.Set(&d)
+	*id = d.ID
+	return err
+}
+
+// Filters
+
+type betweenTime struct {
+	col      string
+	from, to *time.Time
+}
+
+func (b betweenTime) SQL() string {
+	return "[" + b.col + "] BETWEEN ? AND ?"
+}
+
+func (b betweenTime) Vars() []interface{} {
+	return []interface{}{b.from, b.to}
+}
+
+type idEqual struct {
+	col string
+	id  *int64
+}
+
+func (i idEqual) SQL() strign {
+	return "[" + i.col + "] = ?"
+}
+
+func (i idEqual) Vars() []interface{} {
+	return []interface{}{b.id}
 }
