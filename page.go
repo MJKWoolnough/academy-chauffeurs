531a6fac5cf235a7d4cacd3f6330d644af587eea
Michael Woolnough <michael.woolnough@gmail.com>
2015-07-10 16:51:29 +0100

    Typo

--- page.go
+++ page.go
@@ -3,7 +3,7 @@ package main
 var pageHTML = []byte(`<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<title>Acadamy Chauffeurs</title>
+		<title>Academy Chauffeurs</title>
 		<link rel="stylesheet" type="text/css" href="style.css" />
 		<script type="text/javascript" src="code.js"></script>
 	</head>
